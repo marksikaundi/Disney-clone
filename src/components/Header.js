7c07e8b42caea4fc035bfc6b53f6326284125d46
marksikaundi <emmanuelmark117@gmail.com>
2022-03-13 21:56:26 +0200

    Added: styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react'
-
+import styled from 'styled-components'
 function Header() {
   return (
-    <div>
+    <Container>
         Header
-    </div>
+    </Container>
   )
 }
 
 export default Header
+
+
+
+const Container = styled.div`
+
+`
