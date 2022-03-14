c8535c9cd931d61d66eb72d70193480d689e6ac5
marksikaundi <emmanuelmark117@gmail.com>
2022-03-14 18:45:57 +0200

    Added: home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Home() {
   return (
-    <div>
+    <Container>
         Home
-    </div>
+    </Container>
   )
 }
 
 export default Home
+
+const Container = styled.main`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+
+  &:before {
+    background: url("/images/home-background.png") center center cover;
+  }
+`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
