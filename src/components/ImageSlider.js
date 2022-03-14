c991a1250d26028cbf8326ad90f7532ddeb1e23d
marksikaundi <emmanuelmark117@gmail.com>
2022-03-14 23:51:41 +0200

    Add: slides

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,12 +1,37 @@
 import React from 'react'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import styled from 'styled-components';
 
 function ImageSlider() {
-  return (
-    <div>
-        ImageSlider
 
-    </div>
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true
+    }
+
+  return (
+    <Corousel {...settings}>
+        <Wrap>
+            <img src="/images/slider-badging.jpg" alt="" />
+        </Wrap>
+
+    </Corousel>
   )
 }
 
 export default ImageSlider
+
+
+const Corousel = styled(Slider)`
+
+`
+
+const Wrap = styled.div`
+
+`
