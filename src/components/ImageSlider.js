4e296e9d51ae24b8bdf36ae2b6f03f0f2cd13f57
marksikaundi <emmanuelmark117@gmail.com>
2022-03-15 00:25:47 +0200

    ImageSlider: finished

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -20,6 +20,10 @@ function ImageSlider() {
         <Wrap>
             <img src="/images/slider-badging.jpg" alt="" />
         </Wrap>
+        <Wrap>
+            <img src="/images/slider-badag.jpg" alt="" />
+        </Wrap>
+
 
     </Corousel>
   )
@@ -29,9 +33,41 @@ export default ImageSlider
 
 
 const Corousel = styled(Slider)`
+    margin-top: 20px;
 
+    ul li button {
+        &:before {
+            font-size: 10px;
+            color: white;
+        }
+    }
+
+    li.slick-active button:before {
+        color: white;
+    }
+
+    .slick-list {
+        overflow: visible;
+    }
+
+    button {
+        z-index: 1;
+    }
 `
 
 const Wrap = styled.div`
+    cursor: pointer;
+    img {
+        border: 4px solid transparent;
+        border-radius: 4px;
+        width: 100%;
+        height: 100%;
+        box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+        rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+        transition-duration: 300ms;
 
+        &:hover {
+            border: 4px solid rgba(249, 249, 249, 0.8);
+        }
+    }
 `
