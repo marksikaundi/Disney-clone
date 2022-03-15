ae51c8ed1a19dd06a6efd6adecd9da57f174c4c2
marksikaundi <emmanuelmark117@gmail.com>
2022-03-15 23:06:34 +0200

    content

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,8 +4,21 @@ import styled from 'styled-components'
 function Movies() {
   return (
     <Container>
-        
-        Movies
+        <h4>Recommended for You</h4>
+        <Content>
+            <Wrap>
+                <img src="https://www.denofgeek.com/wp-content/uploads/2021/11/The-Simpsons-Season-33-Episode-9-mothers-and-other-strangers.jpg?resize=768%2C432" alt="" />
+            </Wrap>
+            <Wrap>
+                <img src="https://www.denofgeek.com/wp-content/uploads/2021/11/The-Simpsons-Season-33-Episode-9-mothers-and-other-strangers.jpg?resize=768%2C432" alt="" />
+            </Wrap>
+            <Wrap>
+                <img src="https://www.denofgeek.com/wp-content/uploads/2021/11/The-Simpsons-Season-33-Episode-9-mothers-and-other-strangers.jpg?resize=768%2C432" alt="" />
+            </Wrap>
+            <Wrap>
+                <img src="https://www.denofgeek.com/wp-content/uploads/2021/11/The-Simpsons-Season-33-Episode-9-mothers-and-other-strangers.jpg?resize=768%2C432" alt="" />
+            </Wrap>
+        </Content>
 
     </Container>
   )
@@ -16,7 +29,24 @@ export default Movies
 const Container = styled.div`
 
 `
+const Content = styled.div`
+    display: grid;
+    grid-gap: 25px;
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+`
+const Wrap = styled.div`
+border-radius: 10px;
+overflow: hidden;
+border: 3px solid rgba(249, 249, 249, 0.1);
+box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+rgb(0 0 0 / 73%) 0px 16px 10px -10px;
 
+img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+`
 
 
 
