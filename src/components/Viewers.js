2a1bfa23bd23996fd4564f7e3f6102a6f52697ef
marksikaundi <emmanuelmark117@gmail.com>
2022-03-15 22:16:23 +0200

    Add: viewers

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -44,6 +44,14 @@ const Wrap = styled.div`
         object-fit: cover;
     }
 
+    &:hover {
+        box-shadow: rgb(0 0 0 / 80%) 0px 40px 50px -16px,
+        rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+        transform: scale(1.05);
+        border-color: rgba(249, 249, 249, 0.8);
+
+    }
+
 `
 
 // ended on 4:00:00
