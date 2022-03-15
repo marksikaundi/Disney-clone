b8ace36ddc30bc763bb715745ef6617d230ad2e3
marksikaundi <emmanuelmark117@gmail.com>
2022-03-15 22:21:23 +0200

    Added: Movies

---
+++ src/components/Movies.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Movies() {
+  return (
+    <Container>
+        
+        Movies
+
+    </Container>
+  )
+}
+
+export default Movies
+
+const Container = styled.div`
+
+`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
