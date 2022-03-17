c12214b3c3a973f9eea95ee12395802390e3b712
marksikaundi <emmanuelmark117@gmail.com>
2022-03-17 22:50:50 +0200

    Working: movies components

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -36,15 +36,24 @@ const Content = styled.div`
 `
 const Wrap = styled.div`
 border-radius: 10px;
+cursor: pointer;
 overflow: hidden;
 border: 3px solid rgba(249, 249, 249, 0.1);
 box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
 rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
 
 img {
     width: 100%;
     height: 100%;
     object-fit: cover;
+}
+
+&:hover {
+    transition scale(1.05);
+    border-color: rgba(249, 249, 249, 0.8);
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+    rgb(0 0 0 / 72%) 0px 30px 22px -10px;
 }
 `
 
