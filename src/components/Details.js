a32941ed22139e66c57c05671bdec5f0a5ff9d86
marksikaundi <emmanuelmark117@gmail.com>
2022-03-17 23:03:55 +0200

    Added: details page

---
+++ src/components/Details.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Details() {
+  return (
+    <div>
+        
+        Details
+
+    </div>
+  )
+}
+
+export default Details
