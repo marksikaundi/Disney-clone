45fb280a6da1cfd2e451672274fbf720755ddd4e
marksikaundi <emmanuelmark117@gmail.com>
2022-03-21 23:19:27 +0200

    Detail: completed

--- src/components/Details.js
+++ src/components/Details.js
@@ -23,13 +23,18 @@ function Details() {
 
             </TailerButton>
             <AddButton>
-
+                <span>+</span>
             </AddButton>
             <GroupWatchButton>
-
+                <img src="/images/group-icon.png" alt="" />
             </GroupWatchButton>
         </Controls>
-
+            <SubTitle>
+                2022 * 7m * Education, Science, Innovstive, Technology
+            </SubTitle>
+            <Description>
+                Technology is the continually developing result of accumulated knowledge and application in all techniques, skills, methods, and processes used in industrial production and scientific research. Technology is embedded in the operation of
+            </Description>
     </Container>
   )
 }
@@ -71,6 +76,7 @@ const ImageTitle = styled.div`
 
 const Controls = styled.div`
     display: flex;
+    align-items: center;
 `
 const PlayButton = styled.button`
 border-radius: 4px;
@@ -78,18 +84,57 @@ font-size: 15px;
 display: flex;
 align-items: center;
 height: 56px;
+background-color: rgb(249, 249, 249);
+border: none;
+padding: 0px 24px;
+margin-right: 22px;
+text-transform: uppercase;
+letter-spacing: 1.8px;
+cursor: pointer;
 
-
+&:hover {
+    background: rgb(198, 198, 198);
+}
 `
 const TailerButton = styled(PlayButton)`
+background: rgb(0, 0, 0, 0.3);
+border: 1px solid rgb(249, 249, 249)
+color: rgb(249, 249, 249);
+text-transform: uppercase;
 
 `
 const AddButton = styled.button`
+width: 44px;
+height: 44px;
+display: flex;
+align-items: center;
+justify-content: center;
+border-radius: 50%;
+border: 2px solid white;
+cursor: pointer;
+margin-right: 16px;
+
+span {
+    font-size: 30px;
+}
 
 `
 
-const GroupWatchButton = styled.button`
+const GroupWatchButton = styled(AddButton)`
+background: rgb(0, 0, 0);
+`
+
+const SubTitle = styled.div`
+color: rgb(249, 249, 249);
+font-size: 15px;
+min-height: 20px;
+margin-top: 30px;
+`
+const Description = styled.div`
+line-height: 1.4;
+font-size: 20px;
+margin-top: 16px;
+color: rgb(249, 249, 249);
 
 `
 
-//  ended on 4:40:41
