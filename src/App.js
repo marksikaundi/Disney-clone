fd2c89a78b822738516be6de9229926ea2ca6ed9
marksikaundi <emmanuelmark117@gmail.com>
2022-03-21 23:24:43 +0200

    Added: login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Login from './components/Login'
 import { 
   BrowserRouter as Router,
   Switch,
@@ -16,6 +17,9 @@ function App() {
       <Router>
         <Header />
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/detail">
             <Details />
           </Route>
